6ce1c10803b4078e01d348f86575072a11cd3884
atsukirasan-web <atsukirasan@gmail.com>
2026-03-16 13:51:04 +0000

    Update dfrobot_visual_encoder.h

--- components/dfrobot_visual_encoder/dfrobot_visual_encoder.h
+++ components/dfrobot_visual_encoder/dfrobot_visual_encoder.h
@@ -3,32 +3,34 @@
 
 class DFRobotVisualEncoder : public Component, public i2c::I2CDevice {
  public:
-  // Automation triggers as member objects (fixes MockObj)
-  Trigger<void()> clockwise_trigger_;
-  Trigger<void()> counter_trigger_;
-  Trigger<void()> press_trigger_;
-  Trigger<void()> long_press_trigger_;
+  // Automation triggers
+  Trigger<void()> *clockwise_trigger_ = new Trigger<void()>();
+  Trigger<void()> *counter_trigger_ = new Trigger<void()>();
+  Trigger<void()> *press_trigger_ = new Trigger<void()>();
+  Trigger<void()> *long_press_trigger_ = new Trigger<void()>();
 
-  // Accessor methods
-  Trigger<void()>* get_clockwise_trigger() { return &clockwise_trigger_; }
-  Trigger<void()>* get_counter_trigger() { return &counter_trigger_; }
-  Trigger<void()>* get_press_trigger() { return &press_trigger_; }
-  Trigger<void()>* get_long_press_trigger() { return &long_press_trigger_; }
+  // Accessor methods for Python to_code()
+  Trigger<void()>* get_clockwise_trigger() { return clockwise_trigger_; }
+  Trigger<void()>* get_counter_trigger() { return counter_trigger_; }
+  Trigger<void()>* get_press_trigger() { return press_trigger_; }
+  Trigger<void()>* get_long_press_trigger() { return long_press_trigger_; }
 
   void setup() override {
     // Initialize I2C encoder and LED ring
   }
 
   void loop() override {
-    // Read encoder state and call triggers:
-    // Example:
-    // if (rotated_clockwise()) clockwise_trigger_.trigger();
-    // if (rotated_counter()) counter_trigger_.trigger();
-    // if (pressed()) press_trigger_.trigger();
-    // if (long_pressed()) long_press_trigger_.trigger();
+    // Replace the following with your actual encoder logic
+    // Example pseudo-code:
+    // int change = read_encoder();
+    // if (change > 0) clockwise_trigger_->trigger();
+    // if (change < 0) counter_trigger_->trigger();
+    // if (button_pressed()) press_trigger_->trigger();
+    // if (button_long_pressed()) long_press_trigger_->trigger();
   }
 
   void set_led_level(float level) {
-    // level = 0-100, scale to LED ring
+    // level is 0-100, scale to LED ring brightness
+    // send I2C command to set LED ring
   }
 };
