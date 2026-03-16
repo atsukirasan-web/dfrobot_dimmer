6e85d63fc43e485185d08608cdb097e92ae61cb0
atsukirasan-web <atsukirasan@gmail.com>
2026-03-16 22:54:52 +0000

    Update binary_sensor.py

--- components/dfrobot_visual_encoder/binary_sensor.py
+++ components/dfrobot_visual_encoder/binary_sensor.py
@@ -3,23 +3,20 @@ import esphome.config_validation as cv
 from esphome.components import binary_sensor
 from esphome.const import CONF_ID
 
-from . import dfrobot_ns, DFRobotVisualEncoder
+from . import DFRobotVisualEncoder
 
 DEPENDENCIES = ["dfrobot_visual_encoder"]
 
-ButtonSensor = dfrobot_ns.class_("ButtonSensor", binary_sensor.BinarySensor)
+CONF_ENCODER_ID = "encoder_id"
 
 CONFIG_SCHEMA = binary_sensor.binary_sensor_schema().extend(
     {
-        cv.GenerateID(): cv.declare_id(ButtonSensor),
-        cv.GenerateID("encoder_id"): cv.use_id(DFRobotVisualEncoder),
+        cv.GenerateID(CONF_ENCODER_ID): cv.use_id(DFRobotVisualEncoder),
     }
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = await binary_sensor.new_binary_sensor(config)
 
-    await binary_sensor.register_binary_sensor(var, config)
-
-    encoder = await cg.get_variable(config["encoder_id"])
+    encoder = await cg.get_variable(config[CONF_ENCODER_ID])
     cg.add(encoder.set_button_sensor(var))
