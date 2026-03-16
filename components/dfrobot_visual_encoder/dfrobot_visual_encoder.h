c8acc71e8894173995adc0cf12207d1b747ba6c7
atsukirasan-web <atsukirasan@gmail.com>
2026-03-16 21:22:27 +0000

    Update dfrobot_visual_encoder.h

--- components/dfrobot_visual_encoder/dfrobot_visual_encoder.h
+++ components/dfrobot_visual_encoder/dfrobot_visual_encoder.h
@@ -6,17 +6,18 @@ namespace dfrobot_visual_encoder {
 
 class DFRobotVisualEncoder : public Component, public i2c::I2CDevice {
  public:
-  Trigger<void()> clockwise_trigger_;
-  Trigger<void()> counter_trigger_;
-  Trigger<void()> press_trigger_;
-  Trigger<void()> long_press_trigger_;
+  Trigger<> clockwise_trigger_;
+  Trigger<> counter_trigger_;
+  Trigger<> press_trigger_;
+  Trigger<> long_press_trigger_;
 
-  Trigger<void()>* get_clockwise_trigger() { return &clockwise_trigger_; }
-  Trigger<void()>* get_counter_trigger() { return &counter_trigger_; }
-  Trigger<void()>* get_press_trigger() { return &press_trigger_; }
-  Trigger<void()>* get_long_press_trigger() { return &long_press_trigger_; }
+  Trigger<> *get_clockwise_trigger() { return &clockwise_trigger_; }
+  Trigger<> *get_counter_trigger() { return &counter_trigger_; }
+  Trigger<> *get_press_trigger() { return &press_trigger_; }
+  Trigger<> *get_long_press_trigger() { return &long_press_trigger_; }
 
   void setup() override;
+  void dump_config() override;
   void loop() override;
 
   void set_led_level(uint8_t brightness);
@@ -27,13 +28,15 @@ class DFRobotVisualEncoder : public Component, public i2c::I2CDevice {
   void update_led_animation_();
 
   uint8_t last_status_{0};
+
   bool button_pressed_{false};
   uint32_t press_time_{0};
 
   uint8_t current_led_level_{0};
   uint8_t target_led_level_{0};
+
   uint32_t last_led_update_{0};
 };
 
-}  // namespace dfrobot_visual_encoder
-}  // namespace esphome
+}  
+}
