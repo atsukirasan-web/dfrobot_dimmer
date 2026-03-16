1c3cebbc0b9f43d561d55db4d56b6e0399cb507c
atsukirasan-web <atsukirasan@gmail.com>
2026-03-16 21:19:52 +0000

    Update dfrobot_visual_encoder.h

--- components/dfrobot_visual_encoder/dfrobot_visual_encoder.h
+++ components/dfrobot_visual_encoder/dfrobot_visual_encoder.h
@@ -1,34 +1,39 @@
 #pragma once
 #include "esphome.h"
 
+namespace esphome {
+namespace dfrobot_visual_encoder {
+
 class DFRobotVisualEncoder : public Component, public i2c::I2CDevice {
  public:
-  // Automation triggers as member objects (fixes MockObj)
   Trigger<void()> clockwise_trigger_;
   Trigger<void()> counter_trigger_;
   Trigger<void()> press_trigger_;
   Trigger<void()> long_press_trigger_;
 
-  // Accessor methods
   Trigger<void()>* get_clockwise_trigger() { return &clockwise_trigger_; }
   Trigger<void()>* get_counter_trigger() { return &counter_trigger_; }
   Trigger<void()>* get_press_trigger() { return &press_trigger_; }
   Trigger<void()>* get_long_press_trigger() { return &long_press_trigger_; }
 
-  void setup() override {
-    // Initialize I2C encoder and LED ring
-  }
+  void setup() override;
+  void loop() override;
 
-  void loop() override {
-    // Read encoder state and call triggers:
-    // Example:
-    // if (rotated_clockwise()) clockwise_trigger_.trigger();
-    // if (rotated_counter()) counter_trigger_.trigger();
-    // if (pressed()) press_trigger_.trigger();
-    // if (long_pressed()) long_press_trigger_.trigger();
-  }
+  void set_led_level(uint8_t brightness);
 
-  void set_led_level(float level) {
-    // level = 0-100, scale to LED ring
-  }
+ protected:
+  void handle_rotation_(uint8_t status);
+  void handle_button_(uint8_t status);
+  void update_led_animation_();
+
+  uint8_t last_status_{0};
+  bool button_pressed_{false};
+  uint32_t press_time_{0};
+
+  uint8_t current_led_level_{0};
+  uint8_t target_led_level_{0};
+  uint32_t last_led_update_{0};
 };
+
+}  // namespace dfrobot_visual_encoder
+}  // namespace esphome
