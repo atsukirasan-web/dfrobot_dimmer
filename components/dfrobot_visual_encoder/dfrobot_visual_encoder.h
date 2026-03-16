c92813315d48b226689240a90ca47086c851a641
atsukirasan-web <atsukirasan@gmail.com>
2026-03-16 22:41:47 +0000

    Update dfrobot_visual_encoder.h

--- components/dfrobot_visual_encoder/dfrobot_visual_encoder.h
+++ components/dfrobot_visual_encoder/dfrobot_visual_encoder.h
@@ -1,5 +1,9 @@
 #pragma once
+
 #include "esphome.h"
+#include "esphome/components/i2c/i2c.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
 
 namespace esphome {
 namespace dfrobot_visual_encoder {
@@ -12,7 +16,17 @@ class DFRobotVisualEncoder : public Component, public i2c::I2CDevice {
 
   void set_led_level(uint8_t brightness);
 
+  void set_rotation_sensor(sensor::Sensor *sensor) { rotation_sensor_ = sensor; }
+  void set_button_sensor(binary_sensor::BinarySensor *sensor) { button_sensor_ = sensor; }
+
  protected:
+  void handle_rotation_(uint8_t status);
+  void handle_button_(uint8_t status);
+  void update_led_animation_();
+
+  sensor::Sensor *rotation_sensor_{nullptr};
+  binary_sensor::BinarySensor *button_sensor_{nullptr};
+
   uint8_t last_status_{0};
 
   bool button_pressed_{false};
@@ -24,5 +38,5 @@ class DFRobotVisualEncoder : public Component, public i2c::I2CDevice {
   uint32_t last_led_update_{0};
 };
 
-}
+}  
 }
