269ae4c12042b49a3e6efeb004dfd4f25ebf1680
atsukirasan-web <atsukirasan@gmail.com>
2026-03-16 13:14:06 +0000

    Update __init__.py

--- components/dfrobot_visual_encoder/__init__.py
+++ components/dfrobot_visual_encoder/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c
-from esphome import automation  # use automation from the main esphome module
+from esphome import automation  # 2026+ compatible
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ["i2c"]
