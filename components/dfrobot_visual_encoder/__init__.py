be47998323e9943aacdb181a2b8df3d423d6c098
atsukirasan-web <atsukirasan@gmail.com>
2026-03-16 22:04:59 +0000

    Update __init__.py - minimal

--- components/dfrobot_visual_encoder/__init__.py
+++ components/dfrobot_visual_encoder/__init__.py
@@ -1,61 +1,28 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome import automation
-from esphome.components import i2c
-from esphome.const import CONF_ID
+#pragma once
+#include "esphome.h"
 
-DEPENDENCIES = ["i2c"]
+namespace esphome {
+namespace dfrobot_visual_encoder {
 
-dfrobot_ns = cg.esphome_ns.namespace("dfrobot_visual_encoder")
+class DFRobotVisualEncoder : public Component, public i2c::I2CDevice {
+ public:
+  void setup() override;
+  void dump_config() override;
+  void loop() override;
 
-DFRobotVisualEncoder = dfrobot_ns.class_(
-    "DFRobotVisualEncoder",
-    cg.Component,
-    i2c.I2CDevice,
-)
+  void set_led_level(uint8_t brightness);
 
-CONF_ON_CLOCKWISE = "on_clockwise"
-CONF_ON_COUNTER_CLOCKWISE = "on_counter_clockwise"
-CONF_ON_PRESS = "on_press"
-CONF_ON_LONG_PRESS = "on_long_press"
+ protected:
+  uint8_t last_status_{0};
 
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(CONF_ID): cv.declare_id(DFRobotVisualEncoder),
-            cv.Optional(CONF_ON_CLOCKWISE): automation.validate_automation(single=True),
-            cv.Optional(CONF_ON_COUNTER_CLOCKWISE): automation.validate_automation(single=True),
-            cv.Optional(CONF_ON_PRESS): automation.validate_automation(single=True),
-            cv.Optional(CONF_ON_LONG_PRESS): automation.validate_automation(single=True),
-        }
-    )
-    .extend(cv.COMPONENT_SCHEMA)
-    .extend(i2c.i2c_device_schema(0x54))
-)
+  bool button_pressed_{false};
+  uint32_t press_time_{0};
 
+  uint8_t current_led_level_{0};
+  uint8_t target_led_level_{0};
 
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+  uint32_t last_led_update_{0};
+};
 
-    await cg.register_component(var, config)
-    await i2c.register_i2c_device(var, config)
-
-    if CONF_ON_CLOCKWISE in config:
-        await automation.build_automation(
-            config[CONF_ON_CLOCKWISE], [], var.get_clockwise_trigger()
-        )
-
-    if CONF_ON_COUNTER_CLOCKWISE in config:
-        await automation.build_automation(
-            config[CONF_ON_COUNTER_CLOCKWISE], [], var.get_counter_trigger()
-        )
-
-    if CONF_ON_PRESS in config:
-        await automation.build_automation(
-            config[CONF_ON_PRESS], [], var.get_press_trigger()
-        )
-
-    if CONF_ON_LONG_PRESS in config:
-        await automation.build_automation(
-            config[CONF_ON_LONG_PRESS], [], var.get_long_press_trigger()
-        )
+}
+}
