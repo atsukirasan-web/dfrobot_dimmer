4db27051327537d145bae596862cc3368ce4f207
atsukirasan-web <atsukirasan@gmail.com>
2026-03-16 13:17:30 +0000

    Update dfrobot_visual_encoder.h

--- components/dfrobot_visual_encoder/dfrobot_visual_encoder.h
+++ components/dfrobot_visual_encoder/dfrobot_visual_encoder.h
@@ -1,45 +1,36 @@
 #pragma once
-
-#include "esphome/core/component.h"
-#include "esphome/components/i2c/i2c.h"
-#include "esphome/core/automation.h"
-
-namespace esphome {
-namespace dfrobot_visual_encoder {
+#include "esphome.h"
 
 class DFRobotVisualEncoder : public Component, public i2c::I2CDevice {
  public:
-  void setup() override;
-  void loop() override;
-  void dump_config() override;
+  // Automation triggers
+  Trigger<void()> *clockwise_trigger_ = new Trigger<void()>();
+  Trigger<void()> *counter_trigger_ = new Trigger<void()>();
+  Trigger<void()> *press_trigger_ = new Trigger<void()>();
+  Trigger<void()> *long_press_trigger_ = new Trigger<void()>();
 
-  void set_led_level(uint8_t brightness);
+  // Accessor methods for Python to_code()
+  Trigger<void()>* get_clockwise_trigger() { return clockwise_trigger_; }
+  Trigger<void()>* get_counter_trigger() { return counter_trigger_; }
+  Trigger<void()>* get_press_trigger() { return press_trigger_; }
+  Trigger<void()>* get_long_press_trigger() { return long_press_trigger_; }
 
-  Trigger<> *get_clockwise_trigger() { return &clockwise_trigger_; }
-  Trigger<> *get_counter_trigger() { return &counter_trigger_; }
-  Trigger<> *get_press_trigger() { return &press_trigger_; }
-  Trigger<> *get_long_press_trigger() { return &long_press_trigger_; }
+  void setup() override {
+    // Initialize I2C encoder, LED ring, etc.
+  }
 
- protected:
-  Trigger<> clockwise_trigger_;
-  Trigger<> counter_trigger_;
-  Trigger<> press_trigger_;
-  Trigger<> long_press_trigger_;
+  void loop() override {
+    // Replace the following with your encoder logic
+    // Example pseudo-code:
+    // int change = read_encoder();
+    // if (change > 0) clockwise_trigger_->trigger();
+    // if (change < 0) counter_trigger_->trigger();
+    // if (button_pressed()) press_trigger_->trigger();
+    // if (button_long_pressed()) long_press_trigger_->trigger();
+  }
 
-  uint8_t last_status_{0};
-
-  uint8_t target_led_level_{0};
-  uint8_t current_led_level_{0};
-
-  uint32_t last_led_update_{0};
-
-  bool button_pressed_{false};
-  uint32_t press_time_{0};
-
-  void handle_rotation_(uint8_t status);
-  void handle_button_(uint8_t status);
-  void update_led_animation_();
+  void set_led_level(float level) {
+    // level is 0-100, scale to LED ring brightness
+    // send I2C command to set LED ring
+  }
 };
-
-}  
-}
