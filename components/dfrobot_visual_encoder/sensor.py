dc78ebaa97207b202a8e6b9726ca6498c6a18ae8
atsukirasan-web <atsukirasan@gmail.com>
2026-03-17 00:30:16 +0000

    Update sensor.py

--- components/dfrobot_visual_encoder/sensor.py
+++ components/dfrobot_visual_encoder/sensor.py
@@ -3,20 +3,22 @@ import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_ID
 
-from . import DFRobotVisualEncoder
-
 DEPENDENCIES = ["dfrobot_visual_encoder"]
 
-CONF_ENCODER_ID = "encoder_id"
+dfrobot_ns = cg.esphome_ns.namespace("dfrobot_visual_encoder")
+DFRobotVisualEncoder = dfrobot_ns.class_("DFRobotVisualEncoder", cg.Component)
 
-CONFIG_SCHEMA = sensor.sensor_schema().extend(
+CONFIG_SCHEMA = sensor.sensor_schema(
+    unit_of_measurement="steps",
+    accuracy_decimals=0,
+).extend(
     {
-        cv.GenerateID(CONF_ENCODER_ID): cv.use_id(DFRobotVisualEncoder),
+        cv.GenerateID(): cv.use_id(DFRobotVisualEncoder),
     }
 )
 
-async def to_code(config):
-    var = await sensor.new_sensor(config)
 
-    encoder = await cg.get_variable(config[CONF_ENCODER_ID])
-    cg.add(encoder.set_rotation_sensor(var))
+async def to_code(config):
+    parent = await cg.get_variable(config[CONF_ID])
+    sens = await sensor.new_sensor(config)
+    cg.add(parent.set_rotation_sensor(sens))
